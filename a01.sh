5f5cad8609f968a8d8de86178c374a488f941e5a
Ioan Andrei Bârsan <andrei.ioan.barsan@gmail.com>
2018-09-23 17:35:06 -0400

    Minor additions, plus reformat

--- a01.sh
+++ a01.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 set -eu -o pipefail
+
+N_NODES=4
+
+# TODO make this into a makefile maybe
+mpirun -np "$N_NODES" -machinefile config/local-machine.txt cmake-build-debug/globsum
--- src/a01/globsum.cpp
+++ src/a01/globsum.cpp
@@ -5,31 +5,42 @@
 
 #define MPI_CHECK(exp) mpi_safe_call(exp, __FILE__, __LINE__)
 
+// TODO(andreib): use gflags!
+
 int mpi_safe_call(int ret_code, const std::string &fname, int line) {
-    if (MPI_SUCCESS == ret_code) {
-        return ret_code;
-    }
-    else {
-        // There was an error: throw an exception with a meaningful error message.
-        std::stringstream ss;
-        char err_msg[1024];
-        int len;
-        MPI_Error_string(ret_code, err_msg, &len);
-        ss << "MPI call failed in file " << fname << " on line " << line << " with error code " << ret_code
-                  << "(" << err_msg << ").";
-        throw std::runtime_error(ss.str());
-    }
+  if (MPI_SUCCESS == ret_code) {
+    return ret_code;
+  } else {
+    // There was an error: throw an exception with a meaningful error message.
+    std::stringstream ss;
+    char err_msg[1024];
+    int len;
+    MPI_Error_string(ret_code, err_msg, &len);
+    ss << "MPI call failed in file " << fname << " on line " << line << " with error code " << ret_code
+       << "(" << err_msg << ").";
+    throw std::runtime_error(ss.str());
+  }
+}
+
+/**
+ * Hello world in Open MPI.
+ */
+void hello(int argc, char **argv) {
+  using namespace std;
+  int rank = -1, size = -1;
+  MPI_CHECK(MPI_Init(&argc, &argv));
+  MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+  MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &size));
+  cout << "I'm process " << rank << " of " << size << "." << endl;
+
+  MPI_CHECK(MPI_Finalize());
+}
+
+void simple_communication(int argc, char **argv) {
+
 }
 
 int main(int argc, char **argv) {
-    using namespace std;
-    // TODO(andreib): use gflags!
-    int rank = -1, size = -1;
-    MPI_CHECK(MPI_Init(&argc, &argv));
-    MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-    MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &size));
-    cout << "I'm process " << rank << " of " << size << "." << endl;
 
-    MPI_CHECK(MPI_Finalize());
-    return 0;
+  return 0;
 }
