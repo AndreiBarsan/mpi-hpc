Fix spline computation for sinexp case and add heatmap viz

src/a03/plot_output_2d.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 
 
@@ -15,7 +16,7 @@ def main():
     files = [os.path.join(root, f) for f in os.listdir(root) if 'output-problem' in f]
 
     # Little hack for debugging XXX
-    files = [f for f in files if '38' in f]
+    # files = [f for f in files if '38' in f]
 
     for file in files:
         data = json.load(open(file, 'r'))
@@ -25,7 +26,29 @@ def main():
 
         x = data['x']
         gt_y = data['gt_y']
-        interp_y = data['interp_y']
+        interp_y = np.array(data['interp_y'])
+
+        gt_y = np.array(gt_y)
+        gt_y = gt_y.reshape((m+1)*3, (n+1)*3)
+        interp_y = interp_y.reshape((m+1)*3, (n+1)*3)
+
+        delta = interp_y - gt_y
+
+        plt.figure(figsize=(18, 6))
+        plt.subplot(1, 3, 1)
+        sns.heatmap(gt_y)
+        plt.title("Ground truth values")
+        plt.subplot(1, 3, 2)
+
+        sns.heatmap(interp_y)
+        plt.title("Interpolation result")
+
+        plt.subplot(1, 3, 3)
+        sns.heatmap(delta)
+        plt.title("Delta / Error")
+
+        plt.suptitle("Experiment: {}".format(data['name']))
+        plt.show()
 
         # plt.figure()
         # plt.scatter(data['control_x'], data['control_y'], label="Knots")